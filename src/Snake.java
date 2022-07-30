3b8bee13cbc91afcc24e4fdbfe2f8a97082d8bc3
JoelJuaristi <joeljuaristi@hotmail.com>
2022-07-30 12:45:55 +0200

    feat: Added custom exception and real movements

--- src/Snake.java
+++ src/Snake.java
@@ -23,67 +23,101 @@ class Snake
         positions.add(position);
     }
 
-    // TODO this method will be changed to a private method called by four others (moveLeft, moveRight, moveUp, MoveDown)
-    public Boolean move(Integer xMovement, Integer yMovement)
+    public Boolean moveLeft() throws Snake.SnakeException
     {
-        Position head = positions.get(0);
+        return move(-1,0);
+    }
+    public Boolean moveRight() throws Snake.SnakeException
+    {
+        return move(1,0);
+    }
+    public Boolean moveUp() throws Snake.SnakeException
+    {
+        return move(0,-1);
+    }
+    public Boolean moveDown() throws Snake.SnakeException
+    {
+        return move(0,1);
+    }
 
-        if(xMovement > 0)
-        {
-            // Moves to the right
-            if( head.x != lengthX )
-            {
-                head.x = head.x + xMovement; 
-            }
-            else
-            {
-                // In the border and tries to move that way
-                return false;
-            }
-        }
-        else if (xMovement < 0)
-        {
-            // Moves to the left
-            if( head.x != 0 )
-            {
-                head.x = head.x + xMovement; 
-            }
-            else
-            {
-                // In the border and tries to move that way
-                return false;
-            }
-        }
+    private Boolean move(Integer xMovement, Integer yMovement) throws Snake.SnakeException
+    {
 
-        if(yMovement > 0)
+        if(xMovement != 0 && yMovement != 0)
         {
-            // Moves upwards
-            if( head.y != lengthY )
-            {
-                head.y = head.y + xMovement; 
-            }
-            else
-            {
-                // In the border and tries to move that way
-                return false;
-            }
+            // Both parameters are active, snake can't move diagonally
+            throw new SnakeException("Snake can't move diagonally!!");
         }
-        else if (yMovement < 0)
+        else
         {
-            // Moves downwards
-            if( head.y != 0 )
+            // Using directly positions.get(0) keeps the reference so we need a new instance
+            Position head = new Position(positions.get(0).x, positions.get(0).y);
+    
+            if(xMovement > 0)
             {
-                head.y = head.y + yMovement; 
+                // Moves to the right
+                if( head.x != lengthX )
+                {
+                    head.x = head.x + xMovement; 
+                }
+                else
+                {
+                    // In the border and tries to move that way
+                    return false;
+                }
             }
-            else
+            else if (xMovement < 0)
             {
-                // In the border and tries to move that way
-                return false;
+                // Moves to the left
+                if( head.x != 0 )
+                {
+                    head.x = head.x + xMovement; 
+                }
+                else
+                {
+                    // In the border and tries to move that way
+                    return false;
+                }
             }
+    
+            if(yMovement > 0)
+            {
+                // Moves upwards
+                if( head.y != lengthY )
+                {
+                    head.y = head.y + xMovement; 
+                }
+                else
+                {
+                    // In the border and tries to move that way
+                    return false;
+                }
+            }
+            else if (yMovement < 0)
+            {
+                // Moves downwards
+                if( head.y != 0 )
+                {
+                    head.y = head.y + yMovement; 
+                }
+                else
+                {
+                    // In the border and tries to move that way
+                    return false;
+                }
+            }
+    
+            positions.add(0, head);
+            positions.remove(positions.size()-1);
+            return true;
         }
+    }
 
-        positions.add(0, head);
-        positions.remove(positions.size()-1);
-        return true;
+    public class SnakeException extends Exception
+    {
+        public SnakeException (String errorMessage)
+        {
+            super(errorMessage);
+        }
     }
 }
