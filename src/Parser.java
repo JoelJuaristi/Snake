a2aed69fa1bed6af68e01a5b18b67f1162f5f983
JoelJuaristi <joeljuaristi@hotmail.com>
2022-07-31 15:13:16 +0200

    feat: Parser and Scanner

---
+++ src/Parser.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Parser {
+
+    public static Position parsePairOfNumbers(String param)
+    {
+        String [] values = param.split(",");
+        return new Position(Integer.valueOf(values[0]), Integer.valueOf(values[1]));
+    }
+}
